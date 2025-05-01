aa599b48b81d5952a13fea320353b284b013d724
Milena Galhardo <milenagalhardo@Milenas-MacBook-Pro.local>
2025-05-01 09:36:30 +0100

    Integrated team management and views

--- core/models.py
+++ core/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
+# --- Teams ---
 class Team(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
-from django.db import models
 
+# --- Health Cards ---
 class HealthCard(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+# --- Sessions ---
 class Session(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.title} on {self.date.strftime('%Y-%m-%d')}"
